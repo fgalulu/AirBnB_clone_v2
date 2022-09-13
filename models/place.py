fa9841ed8d67d3c6770d52b5c31c13eeb81c73d3
fgalulu <fionasonny132@gmail.com>
2022-09-13 18:43:23 +0000

    task 10

--- models/place.py
+++ models/place.py
@@ -5,6 +5,16 @@ from models.base_model import BaseModel
 from sqlalchemy import Column, ForeignKey, String, Integer, Float, Table
 from sqlalchemy.orm import relationship
 
+if models.storage_t == 'db':
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id', onupdate='CASCADE',
+                                            ondelete='CASCADE'),
+                                 primary_key=True),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id', onupdate='CASCADE',
+                                            ondelete='CASCADE'),
+                                 primary_key=True))
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -39,3 +49,25 @@ class Place(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """Initialize place"""
         super().__init__(*args, **kwargs)
+    if models.storage_t != 'db':
+        @property
+        def reviews(self):
+            """getter attribute returns the list of review instances"""
+            from models.review import Review
+            review_list = []
+            all_reviews = models.storage.all(Review)
+            for review in all_reviews.values():
+                if review.place_id == self.id:
+                    review_list.append(review)
+            return review_list
+
+        @property
+        def amenities(self):
+            """getting attributes returns the list of amenities instances"""
+            from models.amenity import Amenity
+            amenity_list = []
+            all_amenities = models.storage.all(Amenity)
+            for amenity in all_amenities.values():
+                if amenity.place.id == self.id
+                    amenity_list.append(amenity)
+            return amenity_list
