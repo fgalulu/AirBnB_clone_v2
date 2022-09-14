ea6e7598a1ebd4ba7657a884b0bd1e290a478ba1
fgalulu <fionasonny132@gmail.com>
2022-09-14 13:19:52 +0000

    city test update

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -4,7 +4,7 @@ from tests.test_models.test_base_model import TestBaseModel
 from models.city import City
 
 
-class test_City(TestBaseModel):
+class TestCity(TestBaseModel):
     """ """
 
     def __init__(self, *args, **kwargs):
@@ -22,3 +22,10 @@ class test_City(TestBaseModel):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+    def test_city_class_docstring(self):
+        """Test for the City class docstring"""
+        self.assertIsNot(City.__doc__, None,
+                         "City class needs a docstring")
+        self.assertTrue(len(City.__doc__) >= 1,
+                        "City class needs a docstring")
