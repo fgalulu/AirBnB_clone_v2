f4ed3190b39c7762a755c686f83a71e17b8dd795
fgalulu <fionasonny132@gmail.com>
2022-09-14 12:22:59 +0000

    test update

--- tests/test_console.py
+++ tests/test_console.py
@@ -2,3 +2,14 @@
 """
 Contains the class TestConsoleDocs
 """
+import unittest
+import console
+from console import HBNBCommand
+
+
+class TestConsole(unittest.TestCase):
+    """Testing documentation of the console"""
+    def test_console_docstring(self):
+        """Test docstring"""
+        self.assertTrue(len(console.__doc__) >= 1,
+                        "console.py needs a docstring")
